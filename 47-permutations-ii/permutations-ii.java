03ac89efb50de4c399a62fc927a24c59df15fe32
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-09-04 21:02:13 -0700

    Time: 33 ms (19.11%) | Memory: 51.9 MB (5.96%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    public static void swap(int[] arr , int idx , int i){
+        int temp = arr[i];
+        arr[i] = arr[idx];
+        arr[idx] = temp;
+
+    }
+    public static void getPermutation(int[] arr , int idx , List<Integer> ll, List<List<Integer>> list){
+        int n = arr.length;
+        if(idx == n){
+            list.add(new ArrayList(ll));
+            return;
+        }
+        for(int i = idx ; i < n ; i++){
+            ll.add(arr[i]);
+            swap(arr , idx , i);
+            getPermutation(arr ,  idx+1 , ll , list);
+            swap(arr , idx , i);
+            ll.remove(ll.size()-1);
+        }
+    }
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        HashSet< List<Integer>> set =  new HashSet<>();
+        List<List<Integer>> list =  new ArrayList<>();
+        List<Integer> ll =  new ArrayList<>();
+        getPermutation(nums, 0 , ll , list );
+        for( List<Integer> i : list){
+            set.add(i);
+        }
+        list.clear();
+         for( List<Integer> i : set){
+            list.add(i);
+        }
+
+        return list;
+    }
+}
