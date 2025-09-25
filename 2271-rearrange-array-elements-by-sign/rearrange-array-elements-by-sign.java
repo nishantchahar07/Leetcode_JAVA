dda16b9a6a99185f663384bca487e59a4623eaf8
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-09-25 18:12:41 +0530

    Time: 3 ms (100.00%) | Memory: 76.9 MB (67.29%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,33 +1,50 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int n =  nums.length;        
-        int[] pos = new int[n/2];
-        int[] neg = new int[n/2];
-        int pi= 0;
-        int ni = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] > 0){
-                pos[pi] = nums[i];
-                pi++;
-            }
-            else{
-                neg[ni] = nums[i];
-                ni++;
-            }
-        }
-        pi =0;
-        ni = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(i%2 == 0){
-                nums[i] = pos[pi];
-                pi++;
-            }
-            else{
-                nums[i] = neg[ni];
-                ni++;
-            }
-        }
+        // int n =  nums.length;        
+        // int[] pos = new int[n/2];
+        // int[] neg = new int[n/2];
+        // int pi= 0;
+        // int ni = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(nums[i] > 0){
+        //         pos[pi] = nums[i];
+        //         pi++;
+        //     }
+        //     else{
+        //         neg[ni] = nums[i];
+        //         ni++;
+        //     }
+        // }
+        // pi =0;
+        // ni = 0;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(i%2 == 0){
+        //         nums[i] = pos[pi];
+        //         pi++;
+        //     }
+        //     else{
+        //         nums[i] = neg[ni];
+        //         ni++;
+        //     }
+        // }
 
-        return nums;
+        // return nums;
+
+        int n = nums.length;
+        int pi= 0;
+         int ni = 1;
+         int[] ans =  new int[n];
+         for(int x : nums){
+            if(x > 0){
+                ans[pi] = x;
+                pi+=2;
+            }
+            else{
+                ans[ni] = x;
+                ni+=2;
+            }
+         }
+         return ans;
+
     }
 }
