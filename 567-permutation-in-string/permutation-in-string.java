1a1306a03ea698ae615863692462a7600ac04207
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-10-02 18:00:14 +0530

    Time: 27 ms (31.92%) | Memory: 44.7 MB (27.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,32 +1,38 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-       HashMap<Character , Integer> map1 =  new HashMap<>();
-       HashMap<Character , Integer> map2 =  new HashMap<>();
+        HashMap<Character , Integer> map1 = new HashMap<>();
+        HashMap<Character , Integer> map2 = new HashMap<>();
+        
+        int len = s1.length();        
 
-       for(int i = 0 ; i < s1.length(); i++){
-           map1.put(s1.charAt(i) , map1.getOrDefault(s1.charAt(i) , 0)+1);
-
-       }
-       int len =  s1.length();
-       int st = 0;
-       int end = 0;
-         while(end < s2.length() ){           
-         map2.put(s2.charAt(end) , map2.getOrDefault(s2.charAt(end) , 0)+1);
-
-        if(end-st+1 == len){
-            if(map2.equals(map1))return true;
-            char ch = s2.charAt(st);
-         map2.put(ch , map2.get(ch)-1);
-         if(map2.get(ch) == 0 )map2.remove(ch);
-            
-            st++;
-
+        // frequency of s1
+        for (int i = 0; i < len; i++) {
+            map1.put(s1.charAt(i), map1.getOrDefault(s1.charAt(i), 0) + 1);
         }
-         end++;  
-         }
-     
+
+        int st = 0;
+        int end = 0;
+
+        while (end < s2.length()) {
+            map2.put(s2.charAt(end), map2.getOrDefault(s2.charAt(end), 0) + 1);
+
+            // shrink window if larger than len
+            if (end - st + 1 > len) {
+                map2.put(s2.charAt(st), map2.get(s2.charAt(st)) - 1);
+                if (map2.get(s2.charAt(st)) == 0) {
+                    map2.remove(s2.charAt(st));
+                }
+                st++;
+            }
+
+            // check permutation
+            if (end - st + 1 == len && map1.equals(map2)) {
+                return true;
+            }
+
+            end++;
+        }
+
         return false;
-
-
     }
 }
