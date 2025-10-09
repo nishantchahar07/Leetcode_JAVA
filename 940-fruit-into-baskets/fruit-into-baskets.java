d321b7c8ade4b00081cee28643c05151496f5c87
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-10-09 14:11:00 +0530

    Time: 54 ms (19.61%) | Memory: 55.4 MB (32.18%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,24 +1,26 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer , Integer> map =  new HashMap<>();
-        int end  = 0;
+        HashMap<Integer , Integer > map =  new HashMap<>();
         int st = 0;
-        int ans = 0;
-        int maxans = 0;
+        int end = 0;
+        int ans  = 0;
+        int max = 0;       
         while(end < fruits.length){
-            map.put(fruits[end] , map.getOrDefault(fruits[end] , 0)+1);
-            
-            while(map.size() > 2){
-                map.put(fruits[st] , map.get(fruits[st])-1);
-                if(map.get(fruits[st])== 0)map.remove(fruits[st]);
-              
-                st++;
+            int ch  = fruits[end];
+            map.put(ch  , map.getOrDefault(ch , 0) + 1);
+            while(st <= end && map.size() > 2){
+               map.put( fruits[st] , map.get(fruits[st]) - 1);
+               if(map.get(fruits[st]) == 0){             
+                map.remove(fruits[st]);
+               }
+               st++;
+
             }
-            ans = end-st+1;
-            maxans = Math.max(ans , maxans);
+            ans  =  end-st+1;
+            max =  Math.max(ans , max);
             end++;
-            
         }
-        return maxans;
+
+        return max;
     }
 }
