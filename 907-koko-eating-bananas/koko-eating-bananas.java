9821dc715c47baa1f148c890104414b6621235cc
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-10-10 10:26:36 +0530

    Time: 10 ms (66.61%) | Memory: 45.4 MB (34.63%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,32 +1,30 @@
+import java.util.*;
+
 class Solution {
-    public static boolean caneat(int[] arr , int mid , int h){
-        long  count = 0;
-        for(int i = 0 ; i < arr.length;i++){
-            if(arr[i] <= mid)count++;
-            else if(arr[i] > mid ){
-                if(arr[i] % mid == 0){
-                    count+=arr[i]/mid;
-                }
-                else count+=(arr[i]/mid)+1;
-            }
+    public boolean isvalid(int[] arr, long mid, int h) {
+        long count = 0; 
+        for (int i = 0; i < arr.length; i++) {
+            count += (arr[i] + mid - 1) / mid; 
+            if (count > h) return false; 
         }
         return count <= h;
     }
-    public int minEatingSpeed(int[] piles, int h) {
-       int st = 1;
-       int end  = Arrays.stream(piles).max().getAsInt();
-       int ans = 0;
-       while(st <=  end) {
-        int mid  = st + (end-st)/2;
-        if(caneat(piles , mid , h)){
-            ans = mid;
-            end =  mid - 1;
-        
+
+    public int minEatingSpeed(int[] arr, int h) {
+        long st = 1; 
+        long end = Arrays.stream(arr).max().getAsInt();
+        long ans = end;
+
+        while (st <= end) {
+            long mid = st + (end - st) / 2; 
+            if (isvalid(arr, mid, h)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                st = mid + 1;
+            }
         }
-        else{
-            st =  mid + 1;
-        }
-       }
-       return ans;
+
+        return (int) ans; 
     }
 }
