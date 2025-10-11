7d8306296596d1bd76faedea0dabfaa5ef8a0593
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-10-11 13:38:46 +0530

    Time: 23 ms (16.28%) | Memory: 57.4 MB (73.56%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,42 +1,37 @@
 class Solution {
-    public static boolean isvalid(int[] arr, int mid, int m, int k) {
-        int count = 0;  
-        int bouquets = 0; 
-        
-        for (int i : arr) {
-            if (i <= mid) {   
+    public static boolean isvalid(int[] arr , int mid , int m ,int k){
+        int count = 0;
+        int i = 0;
+        while(i < arr.length){
+            if(arr[i] <= mid){
                 count++;
-                if (count == k) {
-                    bouquets++;
-                    count = 0; 
+                if(count == k){
+                    m--;
+                    count = 0;
                 }
-            } else {
-                count=0;
             }
-        }
-        
-        return bouquets >= m;
-    }
-     
-    public int minDays(int[] arr, int m, int k) {
-        
-        if ((long) m * k > arr.length) return -1;
-        
-        int lo = 1;
-        int hi = Arrays.stream(arr).max().getAsInt();
-        int ans = -1;
-        
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
+            else{
+                count = 0;
+            }
             
-            if (isvalid(arr, mid, m, k)) {
-                ans = mid;   
-                hi = mid - 1;
-            } else {
-                lo = mid + 1; 
+            i++;
+        }
+        return m <= 0;
+    }
+    public int minDays(int[] arr, int m, int k) {
+        int st = 0;
+        int end = Arrays.stream(arr).max().getAsInt();
+        int ans = -1;
+        while(st <=  end){
+            int mid = st + (end-st)/2;
+            if(isvalid(arr , mid ,  m , k)){
+                ans =  mid;
+                end = mid-1;
+            }
+            else{
+                st = mid+1;
             }
         }
-        
         return ans;
     }
 }
