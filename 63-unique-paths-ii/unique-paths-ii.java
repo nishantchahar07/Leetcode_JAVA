87a88e07ab8e5b62254f18b09f6f276f557d3132
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-10-16 09:19:53 +0530

    Time: 7 ms (3.38%) | Memory: 43 MB (6.74%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,24 +1,42 @@
 class Solution {
-    public int unique(int cc , int cr , int er , int ec , int[][]arr , int[][]dp){
-       
-          if(cr ==  er && cc == ec)return 1;
-             if(cr >  er || cc > ec)return 0;
-              if(arr[cr][cc] == 1) return 0;
-              if(dp[cr][cc] != -1)return dp[cr][cc];
-        int bottom = unique(cc , cr+1 , er , ec , arr , dp);
-        int right =  unique(cc+1 ,  cr ,  er , ec ,  arr , dp);
-        return dp[cr][cc] =  bottom + right;
+    HashMap<String, Integer> map = new HashMap<>();
+
+    // public int unique(int cc , int cr , int er , int ec , int[][]arr , int[][]dp){
+    public int unique(int cc, int cr, int er, int ec, int[][] arr) {
+
+        if (cr == er && cc == ec)
+            return 1;
+        if (cr > er || cc > ec)
+            return 0;
+        if (arr[cr][cc] == 1)
+            return 0;
+        // if (dp[cr][cc] != -1)
+        //     return dp[cr][cc];
+        String key  = cr+"-"+cc;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        // int bottom = unique(cc, cr + 1, er, ec, arr, dp);
+        // int right = unique(cc + 1, cr, er, ec, arr, dp);
+        // return dp[cr][cc] = bottom + right;
+        int bottom = unique(cc, cr + 1, er, ec, arr);
+        int right = unique(cc + 1, cr, er, ec, arr);
+        int ans  = bottom+right;
+        map.put(key,ans);
+        return ans;
     }
 
     public int uniquePathsWithObstacles(int[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
-        int[][]dp =  new int[m][n];
-        for(int i = 0 ; i < m ; i++){
-            Arrays.fill(dp[i] , -1);
-        }
+        // int[][]dp =  new int[m][n];
+        // for(int i = 0 ; i < m ; i++){
+        //     Arrays.fill(dp[i] , -1);
+        // }
 
-        if(arr[m-1][n-1] == 1 || arr[0][0] == 1)return 0;
-        return unique(0, 0, m-1, n-1, arr , dp);
+        if (arr[m - 1][n - 1] == 1 || arr[0][0] == 1)
+            return 0;
+        // return unique(0, 0, m-1, n-1, arr , dp);
+        return unique(0, 0, m - 1, n - 1, arr);
     }
 }
