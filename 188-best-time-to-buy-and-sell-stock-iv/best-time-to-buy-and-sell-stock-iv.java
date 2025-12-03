0f4472524b0ed62c03abe2659763d1d106a3b0d6
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-12-03 14:46:14 +0530

    Time: 8 ms (31.98%) | Memory: 45.4 MB (28.87%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int getprofit(int idx , int buy , int[] arr , int[][][]dp , int limit ){
+        if(idx ==  arr.length)return 0;
+        if(limit  == 0 )return 0;
+        if(dp[idx][buy][limit] != -1)return  dp[idx][buy][limit];
+        int profit  = 0;
+        if(buy == 1){
+            int buykaro = -arr[idx] + getprofit(idx+1 ,  0  ,  arr ,dp ,limit );
+            int mattkaro = 0 + getprofit(idx+1 ,  1  ,  arr ,dp ,limit );
+            profit = Math.max(buykaro , mattkaro);
+        }
+        else{
+                  int sellkaro = arr[idx] + getprofit(idx+1 ,  1  ,  arr ,dp ,limit-1);
+                    int mattkaro = 0 + getprofit(idx+1 ,  0 ,  arr ,dp ,limit);
+                profit = Math.max(sellkaro , mattkaro);
+        }
+
+        return dp[idx][buy][limit] = profit;
+
+    }
+    public int maxProfit(int k ,int[] prices) {
+
+        int[][][] dp = new int[prices.length][2][k+1];
+        for(int[][] i : dp){
+            for(int [] arr : i){
+            Arrays.fill(arr ,  -1);
+        }
+        }
+        //getprofit(idx, buy/don'tBuy , prices)
+       return  getprofit( 0  ,  1 , prices , dp , k);
+
+          }
+}
