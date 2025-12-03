c89710a6891a973aeda0fa9e62f258720fb66ded
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-12-03 10:51:55 +0530

    Time: 3 ms (6.96%) | Memory: 85.2 MB (50.41%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,23 +1,38 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n =  prices.length;
+        int n   = prices.length;
     
-    int[]pre =  new int[n];    
-    int[]suf  =  new int[n];
-    int ans = 0;
-    pre[0] = prices[0];
-    suf[n-1] = prices[n-1];
+        int[] left  =  new int[n];
+        int[] right = new int[n];
+        left[0] =  prices[0];
+        right[n-1] =  prices[n-1];
 
-    for(int i = 1; i  < n ; i++){
-        pre[i]= Math.min(prices[i] , pre[i-1]);
-    }
-    
-    for(int i = n-2; i  >= 0 ; i--){
-        suf[i]= Math.max(prices[i] , suf[i+1]);
-    }
-    for(int i = 0 ; i < n ; i++){
-        ans = Math.max(ans , suf[i]-pre[i]);
-    }
-    return ans;
+        for(int i = 1 ; i < n ; i++){
+                if(prices[i] < left[i-1]){
+                    left[i] = prices[i];
+                }
+                else{
+                    left[i] =  left[i-1];
+                }
+        }
+        for(int i = n-2 ;i >= 0 ; i--){
+             if(prices[i]  > right[i+1]){
+                    right[i] = prices[i];
+                }
+                else{
+                    right[i] =  right[i+1];
+                }
+        }
+
+        int max = 0;
+        for(int i = 0 ; i < n ; i++){
+            if(max < (right[i] - left[i])){
+                max = right[i] - left[i];
+            }
+        }
+        
+return max;
+        
+
     }
 }
