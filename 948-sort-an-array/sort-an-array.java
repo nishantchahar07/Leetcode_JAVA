52214e8be6d3ca01731333e89c55ba4e87d258e5
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-12-08 16:03:12 +0530

    Time: 24 ms (75.58%) | Memory: 85.3 MB (23.78%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,6 +1,32 @@
 class Solution {
+    public int[] merge(int[] arr1 ,  int[] arr2){
+        int i = 0, j = 0, k = 0;
+        int[] arr = new int[arr1.length + arr2.length];
+
+        while(i < arr1.length && j < arr2.length){
+            if(arr1[i] <= arr2[j]) arr[k++] = arr1[i++];
+            else arr[k++] = arr2[j++];
+        } 
+        while(i < arr1.length) arr[k++] = arr1[i++];
+        while(j < arr2.length) arr[k++] = arr2[j++];
+
+        return arr;
+    }
+
+    public int[] sort(int[] nums, int st, int end){
+        if(st >= end){                       
+            return new int[]{ nums[st] };
+        }
+
+        int mid = st + (end - st) / 2;
+
+        int[] a = sort(nums, st, mid);        
+        int[] b = sort(nums, mid + 1, end);   
+
+        return merge(a, b);
+    }
+
     public int[] sortArray(int[] nums) {
-      Arrays.sort(nums);
-      return nums;
+        return sort(nums, 0, nums.length - 1);   
     }
 }
