53935aeb2ad709df5740faaec644482cf68ad1ef
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2025-12-31 11:09:10 +0530

    Time: 3 ms (33.13%) | Memory: 48.6 MB (26.14%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,42 +1,41 @@
 class Solution {
-    public void dfs(int src , ArrayList<ArrayList<Integer>> adj  ,  boolean[] visited ){
+    public void dfs(int src, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
         visited[src] = true;
-
-        for(int i : adj.get(src)){
-            if(!visited[i]){
-               
-                dfs(i ,  adj ,  visited);
+        for (int i : adj.get(src)) {
+            if (!visited[i]) {
+                dfs(i, adj, visited);
             }
         }
 
     }
+
     public int findCircleNum(int[][] Conn) {
-        int n  =  Conn.length;
-        ArrayList<ArrayList<Integer>> adj  =  new ArrayList<>();
-        for(int i = 0 ; i < Conn.length ; i++ ){
-            adj.add(new ArrayList<>());            
+        int n = Conn.length;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < Conn.length; i++) {
+            adj.add(new ArrayList<>());
         }
 
-    // change the adj matrix to adj list 
-    for(int  i = 0 ; i < n ; i++){
-        for(int j = 0 ; j < n ; j++){
-            if(Conn[i][j] == 1 &&  i != j){
-                adj.get(i).add(j);
-                adj.get(j).add(i);
+        // change the adj matrix to adj list 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (Conn[i][j] == 1 && i != j) {
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
             }
         }
-    }
-    // now apply dfs/bfs for all nodes
-    boolean[] visited =  new boolean[n];
-    ArrayList<Integer> ll = new ArrayList<>();
-    int count = 0;
-    for(int i = 0 ; i < n ; i++){
-        if(!visited[i]){
-            count++;
-            dfs(i ,  adj ,  visited);
+        // now apply dfs/bfs for all nodes
+        boolean[] visited = new boolean[n];
+        ArrayList<Integer> ll = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i, adj, visited);
+            }
         }
-    }
 
-     return count;   
+        return count;
     }
 }
