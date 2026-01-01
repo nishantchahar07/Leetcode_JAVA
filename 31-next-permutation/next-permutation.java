8c73596eb5c6a0d7aa23b3c3df48b45eb4b98a1d
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-01 13:03:57 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (48.14%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,39 +1,40 @@
 class Solution {
-    public static void reverse(int []arr){
-        int i = 0;
-        int j =  arr.length-1;
-        while(i <= j){
-            int temp = arr[i];
+    public void reverse(int[] arr , int i , int j){
+        while(i < j){
+            int temp =  arr[i];
             arr[i] = arr[j];
-            arr[j] = temp;
+            arr[j] =  temp;
             i++;
             j--;
-        }
-    }
-    public void nextPermutation(int[] arr) {
-      int n =  arr.length;
-      int idx = -1;
-      for(int i = n-2 ; i >= 0 ; i--){
-        if(arr[i] < arr[i+1]){
-            idx = i;
-            break;
-        }
-      }
-      System.out.print(idx + " ");
-      if(idx == -1) reverse(arr);
-      else{
-        for(int i = n-1 ; i >= 0 ; i--){
-            if(arr[i] > arr[idx] ){
-                int temp =  arr[i];
-                arr[i] =  arr[idx];
-                arr[idx] =  temp;
-                  break;
-            }            
-          
-        }
-        System.out.print(arr[idx]);
-        Arrays.sort(arr,idx+1 , n);
 
         }
+    }
+    public void nextPermutation(int[] nums) {
+        boolean flag =  false;
+       for(int i = nums.length-1 ; i > 0 ; i--){
+        if(nums[i-1] < nums[i]){
+            flag =  true;
+            for(int j = nums.length-1 ; j >= i ; j--){
+                if(nums[i-1] < nums[j]){
+                   int temp =  nums[i-1];
+                   nums[i-1] = nums[j];
+                   nums[j] = temp;
+                    break;
+                  
+                }
+              
+            }
+            reverse(nums , i , nums.length-1);
+             break;
+
+
+        }       
+           
+        
+       }
+       if(!flag){
+         reverse(nums , 0 , nums.length-1);
+       } 
+
     }
 }
