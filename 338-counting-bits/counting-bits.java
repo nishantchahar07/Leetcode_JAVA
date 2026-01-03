40fc148b8d2810921de6a6f48d0afa74c838afef
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-03 08:15:24 +0530

    Time: 2 ms (96.63%) | Memory: 48.7 MB (49.15%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    // public int get(int i ){
+
+    // }
+    public int[] countBits(int n) {
+        int[] arr =  new int[n+1];
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] =  Integer.bitCount(i);
+        }
+        return arr;
+    }
+}
