651e9fcb6c75502cf2fd5b4682d42e131b37496b
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-07 14:13:18 +0530

    Time: 85 ms (28.78%) | Memory: 75.5 MB (20.32%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,27 +1,32 @@
-class Solution {
-    public boolean get(int[] nums ,  int sum , int n , int count  , Boolean[][]dp ){
-     
-        if(count ==  sum)return true ;
-        if(n == 0) return false;
- if(dp[sum][n] != null)return dp[sum][n];
-        boolean add =  get(nums , sum-nums[n-1] , n-1 , count+nums[n-1] , dp);
-        boolean not = get(nums , sum , n-1 , count , dp);
+class Solution {    
 
-        return dp[sum][n] =  add || not;
-    }
-    public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for(int i : nums){
-            sum+=i;
+    public boolean get(int[] nums, int n, int target, Boolean[][] dp) {
+
+        if(target == 0) return true;   // subset mil gaya
+        if(n == 0) return false;       // items khatam
+
+        if(dp[n][target] != null) return dp[n][target];
+
+        boolean not = get(nums, n-1, target, dp);
+        boolean add = false;
+
+        if(nums[n-1] <= target) {
+            add = get(nums, n-1, target - nums[n-1], dp);
         }
+
+        return dp[n][target] = add || not;
+    }
+
+    public boolean canPartition(int[] nums) {
+
+        int sum = 0;
+        for(int i : nums) sum += i;
 
         if(sum % 2 != 0) return false;
-        int n =  nums.length;
-        Boolean[][]dp =  new Boolean[sum+1][n+1];
-        for(Boolean[]i : dp){
-            Arrays.fill(i , null);
-        }
-        return get(nums , sum ,  n , 0 ,dp );
-        
+
+        int target = sum / 2;
+        Boolean[][] dp = new Boolean[nums.length + 1][target + 1];
+
+        return get(nums, nums.length, target, dp);
     }
 }
