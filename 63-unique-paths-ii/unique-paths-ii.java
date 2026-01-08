752971853c017b288a008c76f4335c042b68c1ae
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-08 19:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (11.89%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,42 +1,22 @@
 class Solution {
-    HashMap<String, Integer> map = new HashMap<>();
+    public int get(int[][] grid ,  int cc , int cr , int m , int n , int[][] dp){
+        if(cc >= m || cr >= n || grid[cc][cr] == 1)return 0;
+        if(cc == m-1 && cr == n-1)return 1;
+        if(dp[cc][cr] != -1)return dp[cc][cr];
 
-    // public int unique(int cc , int cr , int er , int ec , int[][]arr , int[][]dp){
-    public int unique(int cc, int cr, int er, int ec, int[][] arr) {
+        int right = get(grid , cc+1, cr , m , n , dp);
+        int bottom = get(grid , cc, cr+1 , m , n , dp);
+        return dp[cc][cr] = right+bottom;
 
-        if (cr == er && cc == ec)
-            return 1;
-        if (cr > er || cc > ec)
-            return 0;
-        if (arr[cr][cc] == 1)
-            return 0;
-        // if (dp[cr][cc] != -1)
-        //     return dp[cr][cc];
-        String key  = cr+"-"+cc;
-        if(map.containsKey(key)){
-            return map.get(key);
-        }
-        // int bottom = unique(cc, cr + 1, er, ec, arr, dp);
-        // int right = unique(cc + 1, cr, er, ec, arr, dp);
-        // return dp[cr][cc] = bottom + right;
-        int bottom = unique(cc, cr + 1, er, ec, arr);
-        int right = unique(cc + 1, cr, er, ec, arr);
-        int ans  = bottom+right;
-        map.put(key,ans);
-        return ans;
+
     }
-
-    public int uniquePathsWithObstacles(int[][] arr) {
-        int m = arr.length;
-        int n = arr[0].length;
-        // int[][]dp =  new int[m][n];
-        // for(int i = 0 ; i < m ; i++){
-        //     Arrays.fill(dp[i] , -1);
-        // }
-
-        if (arr[m - 1][n - 1] == 1 || arr[0][0] == 1)
-            return 0;
-        // return unique(0, 0, m-1, n-1, arr , dp);
-        return unique(0, 0, m - 1, n - 1, arr);
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m =  grid.length;
+        int n =  grid[0].length;
+        int[][] dp =  new int[m][n];
+        for(int[] i : dp){
+            Arrays.fill(i , -1);
+        }
+        return get(grid , 0, 0 , m , n , dp);
     }
 }
