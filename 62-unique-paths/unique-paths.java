1eb9859fad199b0346b1af09b789eaf23efc7769
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-08 19:26:02 +0530

    Time: 7 ms (1.37%) | Memory: 43.5 MB (5.34%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,18 +1,20 @@
 class Solution {
-    public int maze(int cc , int cr , int er , int ec , int[][]dp ){
+    static HashMap<String , Integer> map =  new HashMap<>(); 
+    public int get(int cc  , int cr , int m , int n){
+        if(cc >= m  || cr >= n)return 0;    
+        if(cc == m-1 && cr == n-1)return 1;
+        String  key = cc+"-"+cr;
+        if(map.containsKey(key))return map.get(key);
 
+        int right =  get( cc+1 , cr ,  m , n );
+        int bottom =  get( cc , cr+1 ,  m , n );
+        int ans = right+bottom;
+        map.put(key ,  ans );
+        return ans;
 
-        if(cr > er || cc > ec)return 0;
-        if( cr ==  er &&cc == ec)return 1;
-        if(dp[cr][cc] != 0)return dp[cr][cc];
-
-        int right = maze(cc +1 , cr , er ,ec , dp);
-        int bottom  = maze(cc , cr+1 , er ,ec , dp);
-        return dp[cr][cc] =  right + bottom;
     }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m+1][n+1];
-        return maze(0 , 0 , m-1, n-1 ,  dp);
-
+        map.clear();
+        return get( 0 , 0 ,  m , n );
     }
 }
