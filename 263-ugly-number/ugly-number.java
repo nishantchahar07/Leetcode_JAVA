c127892df9696431d183419d8b16b2a949e9f0d0
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-13 06:33:11 +0530

    Time: 1 ms (99.21%) | Memory: 42.7 MB (18.21%) - LeetSync

---
+++ 263-ugly-number/ugly-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if (n == 0)
+            return false;
+       
+        while (true) {
+            if (n % 2 != 0)
+                break;
+            n /= 2;
+        }
+
+        
+        while (true) {
+            if (n % 3 != 0)
+                break;
+            n /= 3;
+        }
+
+       
+        while (true) {
+            if (n % 5 != 0)
+                break;
+            n /= 5;
+        }
+
+        return (n == 1);
+    }
+}
