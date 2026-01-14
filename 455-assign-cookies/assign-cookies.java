decb12fe7aae9fb691fd6f4dc9417760343474bf
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-14 12:51:37 +0530

    Time: 15 ms (61.79%) | Memory: 51.8 MB (10.57%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int gi=0;
+        int si=0;
+
+        int count=0;
+
+        while(gi<g.length && si<s.length){
+            if(g[gi]<=s[si]){
+                 count++;
+                 gi++;
+                 si++;
+                 }
+            else si++;
+
+        }
+        return count;
+    }
+}
