75b40c352dbc10084dad04a1fea1ab79316e5862
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-24 18:57:41 +0530

    Time: 258 ms (11.63%) | Memory: 46.8 MB (14.16%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,28 +1,39 @@
 class Solution {
-    public boolean get(int i , int j , int idx , char[][] board, String word){
-        if(word.length() == idx)return true;
-        if(i >=  board.length || j >=  board[0].length || i < 0 || j  < 0 || word.charAt(idx) == '&')return false;
-        if(board[i][j] !=  word.charAt(idx))return false;
-        char ch =  board[i][j];
-        board[i][j] = '&';
-        int[][] dir =  {{1,0} , {-1,0} , {0,-1} , {0,1}};
+    public boolean get(int i , int j , int idx , char[][] board , String word ){
+        if(idx ==  word.length())return true;
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] == '#' )return false;
+    
+         if(board[i][j] != word.charAt(idx)) return false;
+        int[][] dir =  {{1,0} , {0,1} , {-1 , 0} , {0, -1}};
+
+        char val = board[i][j];
+        board[i][j] = '#';
         for(int [] arr : dir){
-           int  new_i =  i + arr[0];
-           int  new_j = j + arr[1];
-           if( get(new_i , new_j , idx+1 ,  board ,  word))return true; 
-         
+           int  new_i = i + arr[0];
+            int new_j = j + arr[1];
+            if(get(new_i , new_j , idx+1 ,   board , word)){
+                return true;
+            }
+             
         }
-            board[i][j] = ch;
-        return false ;
+         board[i][j] = val;
+              return false;
     }
 
-    public boolean exist(char[][] board, String word) {    
-     for(int i = 0 ; i < board.length ; i++){        
-        for(int j = 0 ; j < board[0].length ; j++){
-        if(board[i][j] ==  word.charAt(0) && get(i , j , 0,board ,  word)) 
-        return true;
+
+    public boolean exist(char[][] board, String word) {
+        int n  =  board.length;
+        int m = board[0].length;
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            if(board[i][j] == word.charAt(0) && get(i, j, 0,   board , word)){
+                return true;
+            }
+
         }
-     }
-     return false;
+    }
+    return false;
+    
     }
 }
