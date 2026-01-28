18e9f1f0a312ee66a3b74514e4877d92fca37c42
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-28 21:01:49 +0530

    Time: 1 ms (88.10%) | Memory: 44.1 MB (84.19%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,20 +1,22 @@
 class Solution {
-    public static void get(int idx ,int[] nums ,List<Integer> ll , List<List<Integer>> ans ){
-        if(idx == nums.length){
-            ans.add(new ArrayList<>(ll));
+    public void get(int[] nums ,  List<List<Integer>> ll , List<Integer> ans , int i ){
+        if(i == nums.length){
+            ll.add(new ArrayList<>(ans) );
             return;
         }
-       
-        get(idx+1 ,nums ,  ll, ans );
-        ll.add(nums[idx]);
-        get(idx+1 ,nums ,  ll, ans );
-        ll.remove(ll.size()-1);
+
+        get(nums , ll , ans , i+1);
+        ans.add(nums[i]);
+        get(nums , ll , ans , i+1);
+        ans.remove(ans.size() - 1);
+
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans =  new ArrayList<>();
-        List<Integer> ll =  new ArrayList<>();
-        get(0 , nums ,  ll, ans);
-        return ans;
-        
+       List<List<Integer>> ll = new ArrayList<>();
+       List<Integer> ans =  new ArrayList<>();
+
+       get(nums , ll , ans , 0);
+       return ll;
+
     }
 }
