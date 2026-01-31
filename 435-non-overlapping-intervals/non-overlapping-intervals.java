78a0054d8834c1961cbd1e5e274a264369255d88
Nishant chahar <157943154+nishantchahar07@users.noreply.github.com>
2026-01-31 15:29:38 +0530

    Time: 46 ms (70.59%) | Memory: 115.8 MB (51.65%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -8,19 +8,33 @@ class Solution {
     //     }
     // }
     public int eraseOverlapIntervals(int[][] arr) {
-        Arrays.sort(arr , (a,b) -> a[0]-b[0]);
+        Arrays.sort(arr, (a, b) -> a[1] - b[1]);
         // PriorityQueue<Pair> pq= new PriorityQueue<>((a,b) -> a.b == b.b ? a.a - b.a :  a.b - b.b);
-        int count = 0;
-        for(int i = 0 ; i < arr.length-1 ; i++){
-            if(arr[i+1][0] < arr[i][1] ){
-                count++;
-                if(arr[i+1][1] >= arr[i][1] ){
-                arr[i+1][0] =  arr[i][0];
-                arr[i+1][1] =  arr[i][1];
+        // int count = 0;
+        // for(int i = 0 ; i < arr.length-1 ; i++){
+        //     if(arr[i+1][0] < arr[i][1] ){
+        //         count++;
+        //         if(arr[i+1][1] >= arr[i][1] ){
+        //         arr[i+1][0] =  arr[i][0];
+        //         arr[i+1][1] =  arr[i][1];
+        //     }
+
+        // }
+        // }
+        //    return count ;
+
+        int cnt = 0;
+        int pre = Integer.MIN_VALUE;
+        for (int[] ar : arr) {
+            int a = ar[0];
+            int b = ar[1];
+            if(a < pre){
+                cnt++;
             }
-            
+            else{
+                pre = b;
+            }
         }
-        }
-       return count ;
+        return cnt;
     }
 }
